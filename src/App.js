140f8f1cbde0fdf53ce7f8bfc3a38b6a7275ed7e
Huy <tqhuy1996.developer@gmail.com>
2019-12-19 15:36:06 +0700

    add home and topic component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from './header/header';
+import Home from './pages/home/home';
+import Topics from './pages/topics/topics';
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/topics" component={Topics} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/pages/topics/topics.js
@@ -0,0 +1,15 @@
+import React , {Component} from 'react';
+
+class Topics extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>Topics</div>
+        )
+    }
+}
+
+export default Topics

5dd0f9a2cf67bafc4d29f0ee379f4e0d4a883d26
Huy <tqhuy1996.developer@gmail.com>
2019-12-27 16:08:59 +0700

    fun-project list

--- src/pages/code-fun/fun-project/fun-project.js
+++ src/pages/code-fun/fun-project/fun-project.js
@@ -1,28 +1,45 @@
-import React , {Component} from 'react'
+import React, { Component } from 'react'
 import './fun-project.css';
+import * as firebase from 'firebase';
 export default class FunProjectPage extends Component {
     constructor(props) {
         super(props)
     }
 
+    state = {
+        projects: []
+    }
+
+    componentWillMount() {
+        firebase.firestore().collection("fun-project").get().then((snapshot) => {
+            let dataProject = [];
+            snapshot.forEach((element) => {
+                dataProject.push(
+                    {
+                        id: element.id,
+                        name: element.data().name,
+                        description: element.data().description,
+                        technology: element.data().technology
+                    }
+                )
+            })
+            this.setState({projects: dataProject})
+        })
+    }
+
     render() {
         return (
             <div className="fun-project-container">
-                <div className="fun-project-card">
-                    <div className="fun-project-title">Name Project</div>
-                    <div className="fun-project-description">Description</div>
-                    <div className="fun-project-tech">Technology</div>
-                </div>
-                <div className="fun-project-card">
-                    <div className="fun-project-title">Name Project</div>
-                    <div className="fun-project-description">Description</div>
-                    <div className="fun-project-tech">Technology</div>
-                </div>
-                <div className="fun-project-card">
-                    <div className="fun-project-title">Name Project</div>
-                    <div className="fun-project-description">Description</div>
-                    <div className="fun-project-tech">Technology</div>
-                </div>
+
+                {this.state.projects.map((element) => {
+                    return (
+                        <div key={element.id} className="fun-project-card">
+                            <div className="fun-project-title">{element.name}</div>
+                            <div className="fun-project-description">{element.description}</div>
+                            <div className="fun-project-tech">{element.technology}</div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
